530e5716b218f3b3d101904949f06f30e9f81260
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-21 19:45:50 -0300

    Adding menu "empresa" reference.

--- app/src/main/java/com/cursoandroid/atmempresaconsultoria/MainActivity.java
+++ app/src/main/java/com/cursoandroid/atmempresaconsultoria/MainActivity.java
@@ -3,12 +3,19 @@ package com.cursoandroid.atmempresaconsultoria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView menu_empresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menu_empresa = findViewById(R.id.menu_empresa);
+
     }
 }
